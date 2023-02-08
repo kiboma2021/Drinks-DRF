2fdae5891d0739f79958892f0fce3bdccdfa55e9
kiboma <omayioben@gmail.com>
2023-02-08 10:05:46 +0300

    Add url to render json data

--- Drinks/urls.py
+++ Drinks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from beverages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('softdrinks/', views.softDrinks)
 ]
