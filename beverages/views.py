f3db5a27836f62f251a40ee5ec38c185f5fd1d8d
kiboma <omayioben@gmail.com>
2023-02-08 10:03:54 +0300

    Add end point

--- beverages/views.py
+++ beverages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from serializers import SoftDrinkSerializer
 
-# Create your views here.
+# Get all drinks
+#Serialize them
+#Retun json
+def softDrinks(request):
+    drinks = soft_drink.objects.all()
+    serialized_data=SoftDrinkSerializer(drinks, many=True)
+    return JsonResponse(serialized_data.data, safe=False)
+
+
+
