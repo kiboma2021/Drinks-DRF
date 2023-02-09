21bc033445df1567c9ef3165951d60318d024306
kiboma <omayioben@gmail.com>
2023-02-09 08:24:33 +0300

    Add CRUDE to our serilized data

--- beverages/views.py
+++ beverages/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 from .serializers import *
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Get all drinks
 #Serialize them
 #Retun json
+
+@api_view('GET','POST')
 def softDrinks(request):
-    drinks = soft_drink.objects.all()
-    serialized_data=SoftDrinkSerializer(drinks, many=True)
-    return JsonResponse({'drinks':serialized_data.data})
+    if request.method == 'GET':
+        drinks = soft_drink.objects.all()
+        serialized_data=SoftDrinkSerializer(drinks, many=True)
+        return JsonResponse({'drinks':serialized_data.data})
+    if request.method == 'POST':
+        serialize_data=SoftDrinkSerializer(data=request.data)
+        if serialize_data.is_valid():
+            serialize_data.save()
+            return Response(serialize_data.data, status=status.HTTP_201_CREATED)
 
 def AlcoholDrink(request):
     my_alcohol = alcohol.objects.all()
